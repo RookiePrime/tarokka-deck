4bde6ce48a52e8c62d368fe28d17d936773d56ea
Kiefer <kiefer_8@hotmail.com>
2021-02-24 13:41:01 -0500

    movin' der picturs

--- src/Card.js
+++ src/Card.js
@@ -3,18 +3,17 @@ import './Card.css';
 
 class Card extends Component {
     render() {
-        const {cardData} = this.props;
-        const {imageUrl} = cardData;
+      const {cardData} = this.props;
+      const {imageUrl} = cardData;
 
-        return (
-          <div className="card" style={
+      return (
+        <div className="card" style={
             {
-              backgroundImage: `url(${imageUrl})`
+              backgroundImage: `url("./assets/${imageUrl}")`
             }
           }>
-          </div>
-         );
-
+        </div>
+      );
     }
 }
 
