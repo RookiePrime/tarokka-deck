163b10f4dc0a5c4df913d9f39c58c37c3e34b4ec
Kiefer <kiefer_8@hotmail.com>
2021-03-02 14:19:59 -0500

    Working as intended!

--- src/App.js
+++ src/App.js
@@ -1,78 +1,57 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Card from './Card';
 import deckData from './Deck.json';
 
-class App extends Component {
+const App = () => {
 
-  state = {
-    numberOfCards: 0,
-    cards: []
-  };
+  const [cards, addCards] = useState([]);
+  const [commons, changeCommons] = useState([]);
+  const [highs, changeHighs] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
+    fetch("./Deck.json")
+      .then(() => JSON.parse(JSON.stringify(deckData)))
+      .then(deck => {
+        changeCommons(deck.commonDeck);
+        changeHighs(deck.highDeck);
+      });
+    }, []);
 
-    const loadDeck = () => JSON.parse(JSON.stringify(deckData));
-    const {commonDeck, highDeck} = loadDeck();
-
-    this.setState({
-      commonDeck: commonDeck,
-      highDeck: highDeck
-    });
-  }
-
-  getReading = number => {
-    // Wipe out the current reading
-    // document.getElementById(`cards-box`).innerHTML="";
-    // Get the 3 commons and 2 highs
-    if (this.state.numberOfCards < 3) {
-      this.makeCard(`common`);
-    } else {
-      this.makeCard(`high`);
-    }
-
-    if (number > 3) {
-      document.getElementById(`make-cards`).style.pointerEvents = "none";
-      document.getElementById(`make-cards`).textContent = "";
-    }
-    // Position them on the board
-  }
-
-  makeCard = which => {
-    let daCard = 0;
-    //Ternary for the which
-    which === `common` ? daCard = this.state.commonDeck[Math.floor(Math.random() * this.state.commonDeck.length)] : daCard = this.state.highDeck[Math.floor(Math.random() * this.state.highDeck.length)];
+  const theCards = cards.map( (card, i) => <Card key={cards[i].name} cardData={cards[i]} />);
   
-    daCard.flipped = Math.random() < 0.5;
-    
-    this.setState({
-      numberOfCards: this.state.numberOfCards + 1,
-      cards: this.state.cards.concat(daCard),
-      commonDeck: this.state.commonDeck.filter(card => card !== daCard),
-      highDeck: this.state.highDeck.filter(card => card !== daCard)
-    });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tarokka Reading</h1>
+      </header>
+      <main className="App-main">
+        <button className="make-cards" id="make-cards" onClick={() => addCards(getReading(commons, highs))}>See your Fortune!</button>
+        <div id="cards-box">
+          {theCards}  
+        </div>
+      </main>
+    </div>
+  );
+}
+
+const getReading = (commons, highs) => {
+  const reading = [];
+
+  for (let i = 0; i < 5; i++) {
+    i < 3 ? reading.push(makeCard(commons)) : reading.push(makeCard(highs));
   }
+  console.log(reading);
 
-  render() {
+  return reading;
+}
 
-    const theCards = this.state.cards.map( (card, i) => {
-      return <Card key={i} cardData={this.state.cards[i]} />
-    });
+const makeCard = deck => {
+  const daCard = deck[Math.floor(Math.random() * deck.length)];
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Tarokka Reading</h1>
-        </header>
-        <main className="App-main">
-          <button className="make-cards" id="make-cards" onClick={() => this.getReading(this.state.numberOfCards)}>See your Fortune!</button>
-          <div id="cards-box">
-            {theCards}  
-          </div>
-        </main>
-      </div>
-    );
-  };
+  daCard.flipped = Math.random() > 0.5;
+
+  return daCard;
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,28 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Card.css';
 
-class Card extends Component {
-    render() {
-      const {cardData} = this.props;
-      const {imageUrl, description} = cardData;
+const Card = props => {
+  const {cardData} = props;
+  const {imageUrl, description} = cardData;
 
-      return (
-        <div className="card">
-          <div className="pic-box" style={
+    return (
+      <div className="card">
+        <div className="pic-box" style={
+          {
+            backgroundImage: `url(${process.env.PUBLIC_URL}/assets/card_${imageUrl})`,
+            transform: !cardData.flipped ? `rotate(180deg)` : `rotate(0deg)`
+          }
+        }>
+          <div className="text-box" style={
             {
-              backgroundImage: `url(${process.env.PUBLIC_URL}/assets/card_${imageUrl})`,
-              transform: !cardData.flipped ? `rotate(180deg)` : `rotate(0deg)`
+              transform: !cardData.flipped ? `rotate(180deg)`:  `rotate(0deg)`
             }
-          }>
-            <div className="text-box" style={
-              {
-                transform: !cardData.flipped ? `rotate(180deg)` : `rotate(0deg)`
-              }
-            }><p>{description}</p></div>
-          </div>
+          }><p>{description}</p></div>
         </div>
-      );
-    }
+      </div>
+    );
 }
 
 export default Card;
