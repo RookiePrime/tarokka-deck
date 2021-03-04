18c20e9c0ac706f37d870fa5174a2dad88370b07
Kiefer <kiefer_8@hotmail.com>
2021-03-04 15:58:42 -0500

    Cards now flip! Now okay on phones!

--- src/Card.js
+++ src/Card.js
@@ -3,14 +3,14 @@ import './Card.css';
 
 const Card = props => {
   const {cardData, cardNum} = props;
-  const {imageUrl, description} = cardData;
+  const {description} = cardData;
 
     return (
-      <div className="card" id={`card${cardNum}`}>
+      <div className="card" id={`card${cardNum}`} onClick={() => revealCard(`card${cardNum}`, cardData)}>
+        <div className="order">{cardNum + 1}</div>
         <div className="pic-box" style={
           {
-            backgroundImage: `url(${process.env.PUBLIC_URL}/assets/card_${imageUrl})`,
-            transform: !cardData.flipped ? `rotate(180deg)` : `rotate(0deg)`
+            transform: !cardData.flipped ? `rotate(180deg) scaleX(-1)` : `rotate(0deg) scaleX(-1)`
           }
         }>
           <div className="text-box" style={
@@ -21,6 +21,18 @@ const Card = props => {
         </div>
       </div>
     );
+}
+
+const revealCard = (cardNum, cardData) => {
+  const daCard = document.getElementById(cardNum);
+  const daPicBox = daCard.children[1];
+
+  daCard.className = "card revealed";
+  daCard.children[0].innerHTML = "";
+
+  setTimeout(() => {
+    daPicBox.style.backgroundImage = `url(${process.env.PUBLIC_URL}/assets/card_${cardData.imageUrl})`;
+  }, 200);
 }
 
 export default Card;
